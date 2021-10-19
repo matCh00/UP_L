8dd29da9a97f52736fbfa8b484597803f63ecb6c
matCh00 <mateusz.chalik.g@gmail.com>
2021-10-20 00:14:20 +0200

    Click i TextBox'es

--- WPF_demo/WPF_demo/MainWindow.xaml.cs
+++ WPF_demo/WPF_demo/MainWindow.xaml.cs
@@ -1,14 +1,58 @@
-﻿namespace WPF_demo
+﻿// logika aplikacji
+
+using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WPF_demo
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// new -> .NET Framework -> Desktop Application
-    /// </summary>
     public partial class MainWindow
     {
         public MainWindow()
         {
             InitializeComponent();
+
+            StartFunction();
+        }
+        
+        
+        // pierwsza funkcja
+        public void StartFunction()
+        {
+            AnswerTextBox.Text = "Hello World!";
+        }
+        
+        
+        // funkcje do przycisków - wygenerowana z pliku .xaml
+        
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            
+            // przycisk który został kliknięty
+            var button = sender as Button;
+
+            
+            // akcje po naciśnięciu danego przycisku
+            switch (button.Name)
+            {
+                case "PowerOnCardButton":
+                    AnswerTextBox.Text = "PowerOnCardButton";
+                    break;
+                
+                case "TestButton":
+                    AnswerTextBox.Text += "TestButton";
+                    break;
+                
+                case "CloseButton":
+                    AnswerTextBox.Clear();
+                    break;
+                
+                case "TransmitCommandButton":
+                    AnswerTextBox.Text = CommandTextBox.Text;
+                    break;
+                
+                default: break;
+            }
         }
     }
 }
