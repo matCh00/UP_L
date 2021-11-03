6226e508b4ca7bcf08521b3b5b0d8c21738c4f42
matCh00 <mateusz.chalik.g@gmail.com>
2021-11-03 17:38:04 +0100

    lab2

---
+++ SoundCard/SoundCard/WAVheader.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SoundCard
+{
+    // nagłówek pliku .wav
+    public class WAVheader
+    {
+
+        public byte[] riffID;
+        public uint size;
+        public byte[] wavID;
+        public byte[] fmtID;
+        public uint fmtSize;
+        public ushort format;
+        public ushort channels;
+        public uint sampleRate;
+        public uint bytePerSec;
+        public ushort blockSize;
+        public ushort bit;
+        public byte[] dataID;
+        public uint dataSize;
+
+        public override string ToString()
+        {
+            // rozpoznanie kanału
+            string tempChannel;
+
+            if (channels == 1)
+            {
+                tempChannel = "mono";
+            }
+                
+            else if (channels == 2)
+            {
+                tempChannel = "stereo";
+            }
+                
+            else
+            {
+                tempChannel = "unrecognized";
+            }
+
+            return "riffID: " + riffID + "\n" +
+                   "size: " + size + "\n" +
+                   "fmtSize: " + fmtSize + "\n" +
+                   "dataSize: " + dataSize + "\n" +
+                   "sampleRate: " + sampleRate + "\n" +
+                   "channel: " + channels + " " + tempChannel + "\n";
+        }
+
+    }
+}
