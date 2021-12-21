54d903f6cb9d29767316e36ec633aaf99bfe6c87
matCh00 <mateusz.chalik.g@gmail.com>
2021-12-21 22:14:25 +0100

    commit

---
+++ Camera/Camera/Camera/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using System.Drawing;
+using AForge.Video;
+using AForge.Video.DirectShow;
+
+namespace Camera
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        FilterInfoCollection filterInfoCollection;
+        VideoCaptureDevice videoCaptureDevice;
+
+        private void btnStart_Click(object sender, RoutedEventArgs e)
+        {
+            videoCaptureDevice = new VideoCaptureDevice(filterInfoCollection[cboCamera.SelectedIndex].MonikerString);
+
+            videoCaptureDevice.NewFrame += VideoCaptureDevice_NewFrame;
+
+            videoCaptureDevice.Start();
+        }
+
+        private void VideoCaptureDevice_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            pic.Source = eventArgs.Frame;
+        }
+
+        private void MainWindow_Load(object sender, RoutedEventArgs e)
+        {
+            filterInfoCollection = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+
+            foreach(FilterInfo filterInfo in filterInfoCollection)
+            {
+                cboCamera.Items.Add(filterInfo.Name);
+            }
+
+            cboCamera.SelectedIndex = 0;
+
+            videoCaptureDevice = new VideoCaptureDevice();
+        }
+    }
+}
