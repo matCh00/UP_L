e606f8bdd0e6a57e3f2590e78eac4b7fc285e2be
matCh00 <mateusz.chalik.g@gmail.com>
2021-12-21 22:48:16 +0100

    camera and filters

--- Camera/Camera/Camera/MainWindow.xaml.cs
+++ Camera/Camera/Camera/MainWindow.xaml.cs
@@ -1,7 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -12,8 +16,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-
-using System.Drawing;
+using AForge.Imaging.Filters;
 using AForge.Video;
 using AForge.Video.DirectShow;
 
@@ -21,40 +24,179 @@ namespace Camera
 {
     public partial class MainWindow : Window
     {
+
+        #region Public properties
+
+        public ObservableCollection<FilterInfo> VideoDevices { get; set; }
+
+        public FilterInfo CurrentDevice
+        {
+            get { return _currentDevice; }
+            set { _currentDevice = value; this.OnPropertyChanged("CurrentDevice"); }
+        }
+        private FilterInfo _currentDevice;
+
+        public bool Original
+        {
+            get { return _original; }
+            set { _original = value; this.OnPropertyChanged("Original"); }
+        }
+        private bool _original;
+
+        public bool Grayscaled
+        {
+            get { return _grayscale; }
+            set { _grayscale = value; this.OnPropertyChanged("Grayscaled"); }
+        }
+        private bool _grayscale;
+
+        public bool Thresholded
+        {
+            get { return _thresholded; }
+            set { _thresholded = value; this.OnPropertyChanged("Thresholded"); }
+        }
+        private bool _thresholded;
+
+
+        public int Threshold
+        {
+            get { return _threshold; }
+            set { _threshold = value; this.OnPropertyChanged("Threshold"); }
+        }
+        private int _threshold;
+
+        #endregion
+
+
+        private IVideoSource _videoSource;
+
+
         public MainWindow()
         {
             InitializeComponent();
+
+            this.DataContext = this;
+            GetVideoDevices();
+            Threshold = 127;
+            Original = true;
+            this.Closing += MainWindow_Closing;
         }
 
-        FilterInfoCollection filterInfoCollection;
-        VideoCaptureDevice videoCaptureDevice;
 
+        // zamykanie okna
+        private void MainWindow_Closing(object sender, CancelEventArgs e)
+        {
+            StopCamera();
+        }
+
+
+        // Start (przycisk)
         private void btnStart_Click(object sender, RoutedEventArgs e)
         {
-            videoCaptureDevice = new VideoCaptureDevice(filterInfoCollection[cboCamera.SelectedIndex].MonikerString);
-
-            videoCaptureDevice.NewFrame += VideoCaptureDevice_NewFrame;
-
-            videoCaptureDevice.Start();
+            StartCamera();
         }
 
-        private void VideoCaptureDevice_NewFrame(object sender, NewFrameEventArgs eventArgs)
-        {
-            pic.Source = eventArgs.Frame;
-        }
 
-        private void MainWindow_Load(object sender, RoutedEventArgs e)
+        // nowe wyświetlane klatki
+        private void video_NewFrame(object sender, NewFrameEventArgs eventArgs)
         {
-            filterInfoCollection = new FilterInfoCollection(FilterCategory.VideoInputDevice);
-
-            foreach(FilterInfo filterInfo in filterInfoCollection)
+            try
             {
-                cboCamera.Items.Add(filterInfo.Name);
+                BitmapImage bi;
+                using (var bitmap = (Bitmap)eventArgs.Frame.Clone())
+                {
+                    if (Grayscaled)
+                    {
+                        using (var grayscaledBitmap = Grayscale.CommonAlgorithms.BT709.Apply(bitmap))
+                        {
+                            bi = grayscaledBitmap.ToBitmapImage();
+                        }
+                    }
+                    else if (Thresholded)
+                    {
+                        using (var grayscaledBitmap = Grayscale.CommonAlgorithms.BT709.Apply(bitmap))
+                        using (var thresholdedBitmap = new Threshold(Threshold).Apply(grayscaledBitmap))
+                        {
+                            bi = thresholdedBitmap.ToBitmapImage();
+                        }
+                    }
+                    else  // (Original)
+                    {
+                        bi = bitmap.ToBitmapImage();
+                    }
+                }
+                bi.Freeze();
+                Dispatcher.BeginInvoke(new ThreadStart(delegate { videoPlayer.Source = bi; }));
             }
 
-            cboCamera.SelectedIndex = 0;
+            catch (Exception exc)
+            {
+                MessageBox.Show("Error on _videoSource_NewFrame:\n" + exc.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                StopCamera();
+            }
+        }
 
-            videoCaptureDevice = new VideoCaptureDevice();
+
+        // Stop (przycisk)
+        private void btnStop_Click(object sender, RoutedEventArgs e)
+        {
+            StopCamera();
+        }
+
+
+        // pobranie dostępnych urządzeń
+        private void GetVideoDevices()
+        {
+            VideoDevices = new ObservableCollection<FilterInfo>();
+            foreach (FilterInfo filterInfo in new FilterInfoCollection(FilterCategory.VideoInputDevice))
+            {
+                VideoDevices.Add(filterInfo);
+            }
+            if (VideoDevices.Any())
+            {
+                CurrentDevice = VideoDevices[0];
+            }
+            else
+            {
+                MessageBox.Show("No video sources found", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+
+        // Start kamery
+        private void StartCamera()
+        {
+            if (CurrentDevice != null)
+            {
+                _videoSource = new VideoCaptureDevice(CurrentDevice.MonikerString);
+                _videoSource.NewFrame += video_NewFrame;
+                _videoSource.Start();
+            }
+        }
+
+
+        // Stop kamery
+        private void StopCamera()
+        {
+            if (_videoSource != null && _videoSource.IsRunning)
+            {
+                _videoSource.SignalToStop();
+                _videoSource.NewFrame -= new NewFrameEventHandler(video_NewFrame);
+            }
+        }
+
+
+        // zmany właściwości
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler handler = this.PropertyChanged;
+            if (handler != null)
+            {
+                var e = new PropertyChangedEventArgs(propertyName);
+                handler(this, e);
+            }
         }
     }
 }
