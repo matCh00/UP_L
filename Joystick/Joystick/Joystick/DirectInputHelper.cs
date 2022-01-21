d5de538f3e08d225fb103e44d60f41c8859c1665
matCh00 <mateusz.chalik.g@gmail.com>
2022-01-22 00:33:47 +0100

    coś

---
+++ Joystick/Joystick/Joystick/DirectInputHelper.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SharpDX.DirectInput;
+using System.Threading;
+
+namespace Joystick
+{
+    class DirectInputHelper
+    {
+
+        DirectInput directInput;
+        DeviceInstance gamepadDevice;
+        public int valueX;
+        public int valueY;
+        public int valueTriggers;
+        public bool isButtonClicked;
+
+        public DirectInputHelper()
+        {
+            valueX = 65535 / 2;
+            valueY = 65535 / 2;
+            valueTriggers = 0;
+            isButtonClicked = false;
+            directInput = new DirectInput();
+            gamepadDevice = null;
+        }
+
+        public string getGamepadName()
+        {
+            if (gamepadDevice == null)
+                return "Brak podlaczonego gamepada";
+
+            return gamepadDevice.InstanceName;
+        }
+
+        public void connectGamepad()
+        {
+            gamepadDevice = null;
+            foreach (var deviceInstance in directInput.GetDevices(DeviceType.Gamepad, DeviceEnumerationFlags.AllDevices))
+            {
+                gamepadDevice = deviceInstance;
+                Console.WriteLine(deviceInstance.InstanceName);
+            }
+
+            Thread thread = new Thread(readInput);
+            thread.Start();
+        }
+
+        public void readInput()
+        {
+
+            if (gamepadDevice == null)
+                return;
+
+            SharpDX.DirectInput.Joystick joystick = new SharpDX.DirectInput.Joystick(directInput, gamepadDevice.InstanceGuid);
+            joystick.Properties.BufferSize = 128;
+
+            joystick.Acquire();
+            while (true)
+            {
+                if (joystick.IsDisposed)
+                    return;
+                joystick.Poll();
+                var datas = joystick.GetBufferedData();
+                foreach (var state in datas)
+                {
+                    if (state.Offset.ToString().Equals("X"))
+                    {
+                        valueX = state.Value;
+                        Console.WriteLine(valueX);
+                    }
+                    else if (state.Offset.ToString().Equals("Y"))
+                    {
+                        valueY = state.Value;
+                    }
+                    else if (state.Offset.ToString().Equals("Buttons0"))
+                    {
+                        switch (state.Value)
+                        {
+                            case 0: isButtonClicked = false; break;
+                            case 128: isButtonClicked = true; break;
+                        }
+                    }
+                    else if (state.Offset.ToString().Equals("Z"))
+                    {
+                        valueTriggers = state.Value;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Joystick/Joystick/Joystick/MainWindow.xaml.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Drawing;
+using System.Threading;
+using SharpDX.DirectInput;
+using System.Runtime.InteropServices;
+using System.Windows.Threading;
+
+namespace Joystick
+{
+    public partial class MainWindow : Window
+    {
+
+        DirectInputHelper helper;
+        int progressBarMaxSize = 250;
+        int maxPadValue = 65535;
+        //Graphics g;
+        private const int MOUSEEVENT_LEFTDOWN = 0x02;
+        private const int MOUSEEVENT_LEFTUP = 0x04;
+        int? prevX = null;
+        int? prevY = null;
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            helper = new DirectInputHelper();
+            label4.Width = (int)label4.Width;
+            label4.Height = 0;
+            label5.Width = 0;
+            label5.Height = (int)label5.Height;
+            label7.Width = 0;
+            label7.Height = (int)label5.Height;
+
+            //pictureBox1.BackColor = System.Drawing.Color.White;
+            //g = pictureBox1.CreateGraphics();
+        }
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        internal static extern bool GetCursorPos(ref Win32Point pt);
+
+        [StructLayout(LayoutKind.Sequential)]
+        internal struct Win32Point
+        {
+            public Int32 X;
+            public Int32 Y;
+        };
+        public static System.Drawing.Point GetMousePosition()
+        {
+            var w32Mouse = new Win32Point();
+            GetCursorPos(ref w32Mouse);
+
+            return new System.Drawing.Point(w32Mouse.X, w32Mouse.Y);
+        }
+
+
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        public static extern void mouse_event(int dwFlags, int dx, int dy, int cButtons, int dwExtraInfo);
+
+        private void moveCursor()
+        {
+            this.Dispatcher.Invoke(() =>
+            {
+                /*this.Cursor = new Cursor(Cursor.Current.Handle);
+                Cursor.Position = new System.Drawing.Point(Cursor.Position.X + (int)(4 * ((float)(2 * helper.valueX - maxPadValue) / maxPadValue)),
+                Cursor.Position.Y + (int)(4 * ((float)(2 * helper.valueY - maxPadValue) / maxPadValue)));*/
+                //Cursor.Clip = new Rectangle(this.Location, this.Size);
+            });
+        }
+
+        private void updateProgress()
+        {
+            while (true)
+            {
+                moveCursor();
+                /*if (helper.isButtonClicked)
+                    mouse_event(MOUSEEVENT_LEFTDOWN, Cursor.Position.X, Cursor.Position.Y, 0, 0);
+                else
+                    mouse_event(MOUSEEVENT_LEFTUP, Cursor.Position.X, Cursor.Position.Y, 0, 0);*/
+
+                label4.Dispatcher.Invoke(() =>
+                {
+                    label4.Width = (int)label4.Width;
+                    label4.Height = Convert.ToInt32(progressBarMaxSize * (Convert.ToDouble(helper.valueY) / maxPadValue));
+                    //label4.Background = Brushes.Green;
+                });
+
+                label5.Dispatcher.Invoke(() =>
+                {
+                    label5.Width = Convert.ToInt32(progressBarMaxSize * (Convert.ToDouble(helper.valueX) / maxPadValue));
+                    label5.Height = (int)label5.Height;
+                    //label5.Background = Brushes.Green;
+                });
+
+                checkBox1.Dispatcher.Invoke(() =>
+                {
+                    checkBox1.IsChecked = helper.isButtonClicked;
+                });
+
+                label7.Dispatcher.Invoke(() =>
+                {
+                    label7.Width = Convert.ToInt32(progressBarMaxSize * (Convert.ToDouble(helper.valueTriggers) / maxPadValue));
+                    label7.Height = (int)label7.Height;
+                });
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            helper.connectGamepad();
+            textBox1.Text = helper.getGamepadName();
+            Thread thread = new Thread(updateProgress);
+            thread.Start();
+        }
+
+        private void progressBar2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+           /* Pen p = new Pen(Color.Black, float.Parse("4"));
+            g.DrawLine(p, new Point(prevX ?? e.X, prevY ?? e.Y), new Point(e.X, e.Y));
+            prevX = e.X;
+            prevY = e.Y;*/
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            prevX = null;
+            prevY = null;
+        }
+    }
+}
